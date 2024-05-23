6323adbc0def8efe59655493f7041867c81d6dfb
Blake Bourque <techplex.engineer@gmail.com>
2024-05-22 20:02:01 -0400

    Add bootstrap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,38 @@
 <script>
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import '../app.scss';
 	import Header from './Header.svelte';
 	import './styles.css';
+
+	onMount(async () => {
+		if (!browser) return;
+
+		// this is enough for most components
+		await import('bootstrap');
+
+		// some components require a bootstrap instance, to fulfil their job. In that case, use this:
+		// const bootstrap = await import("bootstrap");
+		// sample usage:
+		// const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+	});
 </script>
 
 <div class="app">
-	<Header />
+	<Header></Header>
 
 	<main>
-		<slot />
+		<slot></slot>
 	</main>
 
 	<footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
+
+<slot></slot>
+
+<slot></slot>
 
 <style>
 	.app {
