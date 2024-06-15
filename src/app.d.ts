91b6b870c6101be8af94adea100924e891a76486
Blake Bourque <techplex.engineer@gmail.com>
2024-06-15 17:12:17 -0400

    D1 Database Integration

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,21 @@
+import type { D1Database } from "@miniflare/d1";
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			rawDb: D1Database;
+		}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				D1_DB?: D1Database;
+			}
+		}
 	}
 }
 
-export {};
+export { };
